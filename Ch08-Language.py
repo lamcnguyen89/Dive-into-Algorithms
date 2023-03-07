e6fe0f437e03118a27de129f1762f61157ba70bf
Lam C Nguyen <lamcnguyen89@gmail.com>
2023-03-07 08:46:25 -0500

    Initial Creation of Chapter 08 File

---
+++ Ch08-Language.py
@@ -0,0 +1,19 @@
+"""
+Chapter 08: Language
+
+In this chapter we will discuss the differences between language and math that make language algorithms difficult.
+
+We will then build a space insertion algorithm that can take any text in any language and insert spacees wherever they are missing.
+
+After that we will build a phase completion algorithm that can imitate the style of a writer and find the most fitting next word in a phrase.
+
+
+We will be relying on two new tools:
+
+    1. List Comprehensions: enable us to quickly generate lists using  the logic of loops and iterations.
+    2. Corpus: A body of text that will "teach" our algorithm the language and style we want it to use.
+
+"""
+
+
+
