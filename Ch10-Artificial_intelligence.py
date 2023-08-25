46da7eb849921540d8d3ecc8b5c8bec4756308f8
Lam C Nguyen <lamcnguyen89@gmail.com>
2023-08-25 17:28:38 -0400

    removed useless comment

--- Ch10-Artificial_intelligence.py
+++ Ch10-Artificial_intelligence.py
@@ -9,8 +9,6 @@ The AI system that we will build will be able to play Dots and Boxes, a simple b
 4. Finally we'll introduce a minmax algorithm.
 """
 
-# Useless Comment
-
 # PAGE 187: Drawing the Board
 
 import matplotlib.pyplot as plt
